d3f0fd53e59b696a51ec25feadfc00e94c2ae334
SiddharthTiwari <siddharthtiwari01@gmail.com>
2019-11-20 11:23:57 +0530

    Add Initial scripts

---
+++ Keras_word_trigger_pretrained.py
@@ -0,0 +1,90 @@
+import os
+#os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import time
+import numpy as np
+from pydub import AudioSegment
+import random
+import sys
+import io
+print("Current File is:",os.path.basename(__file__))
+
+import glob
+import IPython
+from td_utils import *
+
+from keras.callbacks import ModelCheckpoint
+from keras.models import Model, load_model, Sequential
+from keras.layers import Dense, Activation, Dropout, Input, Masking, TimeDistributed, LSTM, Conv1D
+from keras.layers import GRU, Bidirectional, BatchNormalization, Reshape
+from keras.optimizers import Adam
+
+# Load preprocessed training examples
+X = np.load("./XY_train/X.npy")
+Y = np.load("./XY_train/Y.npy")
+
+# Load preprocessed dev set examples
+X_dev = np.load("./XY_dev/X_dev.npy")
+Y_dev = np.load("./XY_dev/Y_dev.npy")
+
+Tx = 5511 # The number of time steps input to the model from the spectrogram
+n_freq = 101 # Number of frequencies input to the model at each time step of the spectrogram
+
+def model(input_shape):
+    """
+    Function creating the model's graph in Keras.
+    
+    Argument:
+    input_shape -- shape of the model's input data (using Keras conventions)
+
+    Returns:
+    model -- Keras model instance
+    """
+    
+    X_input = Input(shape = input_shape)
+    
+    ### START CODE HERE ###
+    
+    # Step 1: CONV layer (≈4 lines)
+    X = Conv1D(196, kernel_size=15, strides=4)(X_input)                                 # CONV1D
+    X = BatchNormalization()(X)                                 # Batch normalization
+    X = Activation('relu')(X)                                 # ReLu activation
+    X = Dropout(0.8)(X)                                 # dropout (use 0.8)
+
+    # Step 2: First GRU Layer (≈4 lines)
+    X = GRU(units = 128, return_sequences = True)(X) # GRU (use 128 units and return the sequences)
+    X = Dropout(0.8)(X)                                 # dropout (use 0.8)
+    X = BatchNormalization()(X)                                 # Batch normalization
+    
+    # Step 3: Second GRU Layer (≈4 lines)
+    X = GRU(units = 128, return_sequences = True)(X)   # GRU (use 128 units and return the sequences)
+    X = Dropout(0.8)(X)                                 # dropout (use 0.8)
+    X = BatchNormalization()(X)                                  # Batch normalization
+    X = Dropout(0.8)(X)                                  # dropout (use 0.8)
+    
+    # Step 4: Time-distributed dense layer (≈1 line)
+    X = TimeDistributed(Dense(1, activation = "sigmoid"))(X) # time distributed  (sigmoid)
+
+    ### END CODE HERE ###
+
+    model = Model(inputs = X_input, outputs = X)
+    
+    return model  
+
+model = model(input_shape = (Tx, n_freq))
+
+print(model.summary())
+
+model = load_model('./models/tr_model.h5')
+
+opt = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, decay=0.01)
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=["accuracy"])
+
+start_time =time.time()
+model.fit(X, Y, batch_size = 5, epochs=10)
+end_time =time.time()
+print("Time Taken for Training:", end_time-start_time)
+
+loss, acc = model.evaluate(X_dev, Y_dev)
+print("Dev set accuracy = ", acc)
+print("--"*25)
